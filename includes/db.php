c9feb901fc50a8ae99d814b215e647c68c7dc782
Krisztian Holub <kholub1989@gmail.com>
2019-12-05 15:09:31 -0500

    insert Data into Category Table and Displaying it

--- includes/db.php
+++ includes/db.php
@@ -10,9 +10,9 @@ foreach ($db as $key => $value) {
 }
 
 $connection = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
-if ($connection) {
-  echo "We are connected";
-}
+// if ($connection) {
+//   echo "We are connected";
+// }
 
 
 ?>
