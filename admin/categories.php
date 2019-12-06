5abbfee18b1c9faa925d217cbce347865d672b4b
Krisztian Holub <kholub1989@gmail.com>
2019-12-06 12:17:47 -0500

    delete Categories

--- admin/categories.php
+++ admin/categories.php
@@ -49,10 +49,6 @@ if (isset($_POST['submit'])) {
           <!-- Add Category Form -->
 
           <div class="col-xs-6">
-<?php 
-$query = "SELECT * FROM categories";
-$select_categories = mysqli_query($connection, $query);
-?>
             <table class="table table-bordered">
               <thead>
                 <tr>
@@ -61,14 +57,26 @@ $select_categories = mysqli_query($connection, $query);
                 </tr>
               </thead>
               <tbody>
-<?php 
+<?php // Find All Categories Query
+$query = "SELECT * FROM categories";
+$select_categories = mysqli_query($connection, $query);
+
 while ($row = mysqli_fetch_assoc($select_categories)) {
   $cat_id = $row['cat_id'];
   $cat_title = $row['cat_title'];
   echo "<tr>";
   echo "<td>{$cat_id}</td>";
   echo "<td>{$cat_title}</td>";
+  echo "<td><a href='categories.php?delete={$cat_id}'>Delete</a></td>";
   echo "</tr>";
+}
+?>
+<?php // Delete Query
+if(isset($_GET['delete'])){
+  $the_cat_id = $_GET['delete'];
+  $query = "DELETE FROM categories WHERE cat_id = {$the_cat_id} ";
+  $delete_query = mysqli_query($connection, $query);
+  header("Location: categories.php");
 }
 ?>
               </tbody>
