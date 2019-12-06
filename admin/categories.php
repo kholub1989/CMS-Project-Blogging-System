179736807fffd39806e4c49e897163ac674898b1
Krisztian Holub <kholub1989@gmail.com>
2019-12-06 11:43:32 -0500

    add prefixes and displaying Data in Categoris page

--- admin/categories.php
+++ admin/categories.php
@@ -1,9 +1,9 @@
-<?php include "includes/header.php" ?>
+<?php include "includes/admin_header.php" ?>
 
 <div id="wrapper">
 
-<!-- Navigation -->
-<?php include "includes/navigation.php" ?>
+  <!-- Navigation -->
+<?php include "includes/admin_navigation.php" ?>
 
   <div id="page-wrapper">
 
@@ -17,36 +17,46 @@
             <small>Author</small>
           </h1>
 
-        <div class="col-xs-6">
-          <form action="">
-            <div class="form-group">
-              <label for="cat_title">Add Category</label>
-              <input class="form-control" type="text" name="cat_title">
-            </div>
-            <div class="form-group">
-              <input class="btn btn-primary" type="submit" name="submit" value="Add Category">
-            </div>
+          <div class="col-xs-6">
+            <form action="">
+              <div class="form-group">
+                <label for="cat_title">Add Category</label>
+                <input class="form-control" type="text" name="cat_title">
+              </div>
+              <div class="form-group">
+                <input class="btn btn-primary" type="submit" name="submit" value="Add Category">
+              </div>
 
-          </form>
-        </div>
-        <!-- Add Category Form -->
+            </form>
+          </div>
+          <!-- Add Category Form -->
 
-        <div class="col-xs-6">
-          <table class="table table-bordered">
-            <thead>
-              <tr>
-                <th>Id</th>
-                <th>Category Title</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td>Baseball Category</td>
-                <td>Baseball Category</td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
+          <div class="col-xs-6">
+<?php 
+$query = "SELECT * FROM categories";
+$select_categories = mysqli_query($connection, $query);
+?>
+            <table class="table table-bordered">
+              <thead>
+                <tr>
+                  <th>Id</th>
+                  <th>Category Title</th>
+                </tr>
+              </thead>
+              <tbody>
+<?php 
+while ($row = mysqli_fetch_assoc($select_categories)) {
+  $cat_id = $row['cat_id'];
+  $cat_title = $row['cat_title'];
+  echo "<tr>";
+  echo "<td>{$cat_id}</td>";
+  echo "<td>{$cat_title}</td>";
+  echo "</tr>";
+}
+?>
+              </tbody>
+            </table>
+          </div>
 
 
         </div>
@@ -59,4 +69,4 @@
   </div>
   <!-- /#page-wrapper -->
 
-  <?php include "includes/footer.php" ?>
+  <?php include "includes/admin_footer.php" ?>
