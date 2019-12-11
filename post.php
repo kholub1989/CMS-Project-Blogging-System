33e9e3d1550630d82452b723aaf77375e7fb43c4
Krisztian Holub <kholub1989@gmail.com>
2019-12-11 12:04:40 -0500

    finish the query to send comment data

--- post.php
+++ post.php
@@ -64,7 +64,13 @@ if(isset($_POST['create_comment'])) {
 
   $query = "INSERT INTO comments (comment_post_id, comment_author, comment_email, comment_content, comment_status, comment_date)";
 
-  $query .= "VALUES ($the_post_id, '{$comment_author}', '{$comment_email}', '{$comment_content}', 'unapproved', now()";
+  $query .= "VALUES ($the_post_id, '{$comment_author}', '{$comment_email}', '{$comment_content}', 'unapproved', now())";
+
+  $create_comment_query = mysqli_query($connection, $query);
+  
+  if(!$create_comment_query){
+    die("QUERY FAILED" . mysqli_error($connection));
+  }
 }
 ?>
 
