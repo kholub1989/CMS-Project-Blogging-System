ee231715b80ca61e5e7f283950aed4a87b237b1f
Krisztian Holub <kholub1989@gmail.com>
2019-12-13 13:54:01 -0500

    add notification to add User page

--- admin/includes/add_user.php
+++ admin/includes/add_user.php
@@ -22,6 +22,9 @@ if(isset($_POST['create_user'])) {
  $create_user_query = mysqli_query($connection, $query);
 
  confirmQuery($create_user_query);
+
+//  echo "User Created: " . " " . "<a href='users.php'>View Users</a>";
+ echo "<div class='alert alert-success' role='alert'>User Created: <a href='users.php'>View Users</a></div>";
 }
 ?>
 <!-- ,'$post_image_temp' -->
