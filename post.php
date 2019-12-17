42a38889c09f884677b96a18e0ebaec281980d70
Krisztian Holub <kholub1989@gmail.com>
2019-12-17 13:54:59 -0500

    adding Validation to Comments in Post Page

--- post.php
+++ post.php
@@ -60,7 +60,8 @@ if(isset($_POST['create_comment'])) {
   $comment_email = $_POST['comment_email'];
   $comment_content = $_POST['comment_content'];
 
-  $query = "INSERT INTO comments (comment_post_id, comment_author, comment_email, comment_content, comment_status, comment_date)";
+  if(!empty($comment_author) && !empty($comment_email) && !empty($comment_content)) {
+    $query = "INSERT INTO comments (comment_post_id, comment_author, comment_email, comment_content, comment_status, comment_date)";
 
   $query .= "VALUES ($the_post_id, '{$comment_author}', '{$comment_email}', '{$comment_content}', 'unapproved', now())";
 
@@ -73,6 +74,9 @@ if(isset($_POST['create_comment'])) {
   $query = "UPDATE posts SET post_comment_count = post_comment_count + 1 ";
   $query .= "WHERE post_id = $the_post_id ";
   $update_comment_count = mysqli_query($connection, $query);
+  } else {
+    echo "<script>alert('Fields cannot be empty')</script>";
+  }
 }
 ?>
 
