d263037d49101d718cb2ab6752570be6d26a1c6b
Krisztian Holub <kholub1989@gmail.com>
2019-12-19 00:07:40 -0500

    displaying users online

--- admin/index.php
+++ admin/index.php
@@ -13,8 +13,13 @@ $send_query = mysqli_query($connection, $query);
 $count = mysqli_num_rows($send_query);
 
 if($count == NULL) {
-  mysqli_query($connection, "INSERT INTO users_online(sessions, time) VALUES('$$session', '$time')");
-} 
+  mysqli_query($connection, "INSERT INTO users_online(session, time) VALUES('$session', '$time')");
+} else {
+  mysqli_query($connection, "UPDATE users_online SET time = '$time' WHERE session = '$session'");
+}
+$users_online_query = mysqli_query($connection, "SELECT * FROM users_online WHERE time > '$time_out'");
+$count_user = mysqli_num_rows($users_online_query);
+
 
 ?>
 
@@ -32,6 +37,11 @@ if($count == NULL) {
             Welcome to admin
             <small><?php echo $_SESSION['username'] ?></small>
           </h1>
+          
+          <h1>
+          <?php echo $count_user; ?>
+          </h1>
+
         </div>
       </div>
       <!-- /.row -->
