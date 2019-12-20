352699e0ee121b3cb3885ab89a148d983b4deb96
Krisztian Holub <kholub1989@gmail.com>
2019-12-20 11:08:59 -0500

    insertion post for specific users

--- admin/includes/view_all_posts.php
+++ admin/includes/view_all_posts.php
@@ -73,7 +73,7 @@
       <tr>
         <th><input id="selectAllBoxes" type="checkbox"></th>
         <th>Id</th>
-        <th>Author</th>
+        <th>Users</th>
         <th>Title</th>
         <th>Category</th>
         <th>Status</th>
@@ -95,6 +95,7 @@
   while ($row = mysqli_fetch_assoc($select_posts)) {
     $post_id = $row['post_id'];
     $post_author = $row['post_author'];
+    $post_user = $row['post_user'];
     $post_title = $row['post_title'];
     $post_category_id = $row['post_category_id'];
     $post_status = $row['post_status'];
@@ -109,7 +110,12 @@
     <td><input class='checkBoxes' type='checkbox' name='chackBoxArray[]' value='<?php echo $post_id; ?>'></td>
 <?php 
     echo "<td>$post_id</td>";
-    echo "<td>$post_author</td>";
+    if(!empty($post_author)) {
+      echo "<td>$post_author</td>";
+    } elseif(!empty($post_user)) {
+      echo "<td>$post_user</td>";
+    }
+
     echo "<td>$post_title</td>";
   
     $query = "SELECT * FROM categories WHERE cat_id = {$post_category_id} ";
