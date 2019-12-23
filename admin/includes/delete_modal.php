425a584c439dc9fc068f9c18a7970cd780f3c22a
Krisztian Holub <kholub1989@gmail.com>
2019-12-23 14:02:10 -0500

    add modal delete functionaleti

--- admin/includes/delete_modal.php
+++ admin/includes/delete_modal.php
@@ -1,6 +1,3 @@
-<!-- Trigger the modal with a button -->
-<button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#myModal">Open Modal</button>
-
 <!-- Modal -->
 <div id="myModal" class="modal fade" role="dialog">
   <div class="modal-dialog">
@@ -12,7 +9,7 @@
         <h4 class="modal-title">Modal Delete Box</h4>
       </div>
       <div class="modal-body">
-        <h3>Are you shure you want to delete this post?</h3>
+        <h3 class="text-center">Are you shure you want to delete this post?</h3>
       </div>
       <div class="modal-footer">
         <a href="" class="btn btn-danger modal_delete_link">Delete</a>
--- admin/includes/view_all_posts.php
+++ admin/includes/view_all_posts.php
@@ -144,8 +144,7 @@
     echo "<td>$post_date</td>";
     echo "<td><a href='../post.php?p_id={$post_id}'>View Post</a></td>";
     echo "<td><a href='posts.php?source=edit_post&p_id={$post_id}'>Edit</a></td>";
-    echo "<td><a rel='$post_id' href='' class='delete_link'>Delete</a></td>";
-
+    echo "<td><a rel='$post_id' href='javascript:void(0)' class='delete_link'>Delete</a></td>";
     // echo "<td><a onClick=\"javascript: return confirm('Are you shure you want to delete?'); \" href='posts.php?delete={$post_id}'>Delete</a></td>";
     echo "<td><a href='posts.php?reset={$post_id}'>{$post_views_count}</a></td>";
     echo "</tr>";
@@ -176,7 +175,10 @@ if(isset($_GET['reset'])) {
   $(document).ready(function(){
     $(".delete_link").on('click', function(){
       var id = $(this).attr("rel");
-      
+      var delete_url = "posts.php?delete=" + id + " ";
+      $(".modal_delete_link").attr("href", delete_url);
+      $("#myModal").modal("show")
+
     })
   });
 </script>
