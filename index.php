64ac1df3936d9a9e6ddcffd8ecd20ef34edd1f9a
Krisztian Holub <kholub1989@gmail.com>
2019-12-23 15:17:12 -0500

    no post feature gome page

--- index.php
+++ index.php
@@ -26,9 +26,13 @@
       $page_1 = ($page * $per_page) - $per_page;
     }
 
-    $post_query_count = "SELECT * FROM posts";
+    $post_query_count = "SELECT * FROM posts WHERE post_status = 'published' ";
     $find_count = mysqli_query($connection, $post_query_count);
     $count = mysqli_num_rows($find_count);
+    if($count < 1) {
+      echo "<h1 class='text-center'>No posts available</h1>";
+    } else {
+
     $count = ceil($count / $per_page);
     
 
@@ -43,9 +47,6 @@
         $post_image = $row['post_image'];
         $post_content = substr($row['post_content'], 0, 100) . '...';
         $post_status = $row['post_status'];
-
-        if ($post_status == 'published') {
-        
     ?>
 
       <h1 class="page-header">
