d540aa44fa8b4c8f4a16dce34b77dcb90cee32d8
Krisztian Holub <kholub1989@gmail.com>
2019-12-23 19:58:58 -0500

    fix bug in our post js editor

--- admin/includes/edit_post.php
+++ admin/includes/edit_post.php
@@ -143,7 +143,7 @@ if(isset($_POST['update_post'])) {
 
   <div class="form-group">
     <label for="post_content">Post Content</label>
-    <textarea class="form-control" name="post_content" id="body" cols="30" rows="10"><?php echo $post_content; ?>
+    <textarea class="form-control" name="post_content" id="body" cols="30" rows="10"><?php echo str_replace('\r\n', '</br>', $post_content); ?>
     </textarea>
   </div>
 
