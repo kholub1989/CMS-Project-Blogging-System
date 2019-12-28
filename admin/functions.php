b9269ab26aff2739bbdee72838a6d5a6463c9fc1
Krisztian Holub <kholub1989@gmail.com>
2019-12-27 22:37:46 -0500

    inserting categories with prepare statments

--- admin/functions.php
+++ admin/functions.php
@@ -53,11 +53,11 @@ function insert_categories() {
     if($cat_title == "" || empty($cat_title)) {
       echo "This field should not be empty";
     } else {
-      $query = "INSERT INTO categories(cat_title) ";
-      $query .= "VALUE('{$cat_title}')";
-  
-      $create_categories_query = mysqli_query($connection, $query);
-      if(!$create_categories_query) {
+      $stmt = mysqli_prepare($connection, "INSERT INTO categories(cat_title) VALUES(?)");
+      mysqli_stmt_bind_param($stmt, "s", $cat_title);
+      mysqli_stmt_execute($stmt);
+      
+      if(!$stmt) {
         die('QUERY FAILED' . mysqli_error($connection));
       }
     }
