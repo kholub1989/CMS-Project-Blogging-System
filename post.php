45c833080e68b81597286a691f21eb9fb32c3d54
Krisztian Holub <kholub1989@gmail.com>
2020-01-08 22:11:17 -0500

    fetching the posts

--- post.php
+++ post.php
@@ -6,7 +6,17 @@
 
 <?php 
  if (isset($_POST['liked'])) {
-   echo "<h1>IT WoRKS</h1>";
+   $post_id = $_POST['post_id'];
+   // Select POST 
+   $query = "SELECT * FROM posts WHERE post_id=$post_id";
+   $postResult = mysqli_query($connection, $query);
+   $post = mysqli_fetch_array($postResult);
+   $likes = $post['likes'];
+   if (mysqli_num_rows($postResult) >= 1) {
+     echo $post['post_id'];
+   }
+   // Update POST with Likes
+   // Create Likes for POST
  }
 ?>
 
