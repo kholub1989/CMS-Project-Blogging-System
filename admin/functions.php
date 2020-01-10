a0a3d11104600fc7a25412fad207aa5801eae24f
Krisztian Holub <kholub1989@gmail.com>
2020-01-10 11:55:43 -0500

    creating a helper func to detect if the user likes a specific post

--- admin/functions.php
+++ admin/functions.php
@@ -44,11 +44,14 @@ function loggedInUserId() {
   if (isLoggedIn()) {
     $result = query("SELECT * FROM users WHERE username='" . $_SESSION['username'] . "'");
     $user = mysqli_fetch_array($result);
-    if (mysqli_num_rows($result) >= 1) {
-      return $user['user_id'];
-    }
+    return mysqli_num_rows($result) >= 1 ? $user['user_id'] : false;
   }
   return false;
+}
+
+function userLikedThisPost($post_id = '') {
+  $result = query("SELECT * FROM likes WHERE user_id=" . loggedInUserId() . " AND post_id={$post_id}");
+  return mysqli_num_rows($result) >= 1 ? true : false;
 }
 
 function checkIfUserisLogegdInAndRedirect($redirectLocation=null) {
--- test.php
+++ test.php
@@ -3,4 +3,10 @@
 <?php include "./admin/functions.php" ?>
 <?php 
 echo loggedInUserId();
+
+if (userLikedThisPost(51)) {
+    echo " USER LIKED IT";
+} else {
+    echo " USER DID NOT LIKE IT";
+}
 ?>
